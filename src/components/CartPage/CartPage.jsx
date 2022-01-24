e96fff0d3bb4c79fcd0b8806f1378caeff9ef2e3
Paulo Andrade <paulobacelara@gmail.com>
2022-01-24 10:24:50 -0300

    Created basic styles and markup for cart page

--- src/components/CartPage/CartPage.jsx
+++ src/components/CartPage/CartPage.jsx
@@ -1,10 +1,24 @@
+import "./CartPage.css";
 import React from "react";
+
+const ErrorMessage = () => {
+  return (
+    <p className="cart__error">
+      Oops... you didn't add any product to your cart :( <br />
+      <a href="/">Go to the shop</a> and check out our amazing products.
+    </p>
+  );
+};
 
 const CartPage = () => {
   return (
-    <>
-      <h1>CART PAGE</h1>
-    </>
+    <section className="cart">
+      <div className="cart__content">
+        <h2 className="cart__title">Cart</h2>
+
+        <ErrorMessage />
+      </div>
+    </section>
   );
 };
 
